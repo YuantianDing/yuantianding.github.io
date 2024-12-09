a8cb0417e255fe8b06c9900bb25109165093dcfa
Yuantian Ding <dnailzb@outlook.com>
2024-12-08 23:22:11 -0500

    on destination

--- zh/src/components/layout/Layout.tsx
+++ zh/src/components/layout/Layout.tsx
@@ -1,7 +1,12 @@
 import Header from '@/components/layout/Header';
 import * as React from 'react';
 
-export default function Layout({ children, title, title_dist }: { title: React.ReactNode, children: React.ReactNode, title_dist?: string }) {
+export default function Layout({ children, title, title_dist, subtitle }: { title: React.ReactNode, children: React.ReactNode, title_dist?: string, subtitle?: React.ReactNode }) {
+  const subtitle_div = (subtitle)? (
+    <div className="subtitle-size mx-auto text-center pt-6 text-indigo-400" style={{height: title_dist ?? '9rem'}}>
+      {subtitle}
+    </div>
+  ): <div style={{height: title_dist ?? '9rem'}}></div>;
   return <div className="bg-color text-white">
     <div>
       <Header />
@@ -10,7 +15,7 @@ export default function Layout({ children, title, title_dist }: { title: React.R
         <div className="grid grid-row-6 items-center justify-center py-12 content-stretch">
             <div style={{height: '5vh', width: '90vw'}}></div> 
             <span className="title-size mx-auto text-center text-white" style={{transform: 'scale(1.0, 1)'}}> {title} </span> 
-            <div style={{height: title_dist ?? '9rem'}}></div>
+            {subtitle_div}
             <div className="items-center justify-center px-3">
               <div className="max-w-6xl center-box">
                 {children}
